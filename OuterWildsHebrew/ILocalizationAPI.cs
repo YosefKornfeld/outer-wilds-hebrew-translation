d99a9006a6b7fc7b0731344365bff76d51aa9057
yosefk7 <yosefk7@gmail.com>
2026-08-11 09:27:11 +0300

    first commit

---
+++ OuterWildsHebrew/ILocalizationAPI.cs
@@ -0,0 +1,12 @@
+using OWML.ModHelper;
+using System;
+
+namespace OuterWildsHebrew
+{
+    public interface ILocalizationAPI
+    {
+        void RegisterLanguage(ModBehaviour mod, string name, string translationPath);
+        void AddLanguageFont(ModBehaviour mod, string name, string assetBundlePath, string fontPath);
+        void AddLanguageFixer(string name, Func<string, string> fixer);
+    }
+}
---
+++ OuterWildsHebrew/OuterWildsHebrew.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using OWML.Common;
+using OWML.ModHelper;
+using System.Reflection;
+
+namespace OuterWildsHebrew;
+
+public class OuterWildsHebrew : ModBehaviour
+{
+	public static OuterWildsHebrew Instance;
+
+	public void Awake()
+	{
+		Instance = this;
+		// You won't be able to access OWML's mod helper in Awake.
+		// So you probably don't want to do anything here.
+		// Use Start() instead.
+	}
+
+	public void Start()
+	{
+	    var api = ModHelper.Interaction.TryGetModApi<ILocalizationAPI>("xen.LocalizationUtility");
+	    if (api != null)
+	    {
+	        api.RegisterLanguage(this, "Hebrew", "assets/Translation.xml");
+	    }
+	}
+
+	public void OnCompleteSceneLoad(OWScene previousScene, OWScene newScene)
+	{
+		if (newScene != OWScene.SolarSystem) return;
+		ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+	}
+}
